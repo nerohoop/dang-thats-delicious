37075cf55cc53dcfb147ad1ccfc3fc4e93330f61
Jiang Xiao <nero.jiang@my-doc.com>
2018-04-23 17:21:44 +0800

    Using Async Await

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
     res.render('index');
@@ -7,6 +9,8 @@ exports.editStore = (req, res) => {
     res.render('editStore', { title: 'Add Store' });
 }
 
-exports.createStore = (req, res) => {
-    res.json(req.body);
+exports.createStore = async (req, res) => {
+    const store = new Store(req.body);
+    await store.save();
+    res.redirect('/');
 }
